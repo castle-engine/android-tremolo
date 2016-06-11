913d1309e85af7933803f4ca2ce56de8589d35be
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-06-11 20:56:39 +0200

    Compile libtremolo.so and libtremolo-low-precision.so

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
 # Easily compile Tremolo as a shared library using Android NDK
 
-"Tremolo" is, in short, a fast native library to read OggVorbis on Android.
+"Tremolo" is (simplifying a little) a fast native library
+to read OggVorbis on Android.
 See http://wss.co.uk/pinknoise/tremolo/ for the details.
 
-Compile by simple `ndk-build` or `make` here.
-(assuming you have Android NDK installed and ndk-build on $PATH).
-This will give you the libtremolo.so, that you
-can use e.g. with Castle Game Engine "ogg_vorbis" component.
+Compile by simple
+
+  ndk-build
+
+or
+
+  make
+
+here. This assumes you have Android NDK installed and `ndk-build` on $PATH.
+This will create files
+
+  jni/libtremolo.so
+  jni/libtremolo-low-precision.so
+
+You can use them in your Android projects. Choose one -- normal, or low precision.
+The API is the same, and is also compatible with (a subset of) libvorbisfile.
+For example you can use these libraries with Castle Game Engine "ogg_vorbis"
+component (see https://github.com/castle-engine/castle-engine/wiki/Android-Project-Components-Integrated-with-Castle-Game-Engine#ogg_vorbis
+), just copy the chosen .so file to `castle-engine/tools/build-tool/data/android/integrated-components/ogg_vorbis/jni/`.
 
 The source code here is taken from Tremolo 0.08 release,
 http://wss.co.uk/pinknoise/tremolo/Tremolo008.zip .
 The idea to arrange it like this, to easily compile using NDK,
-is from http://repo.or.cz/openal-soft/android.git ,
-with some tiny changes to make it compile as a shared lib.
+is from http://repo.or.cz/openal-soft/android.git .
+Michalis only did some tiny changes to `Android.mk` to easily compile
+both shared libraries.
--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ TARGET_PLATFORM := android-3
 ROOT_PATH := $(call my-dir)
 
 ########################################################################################################
-# Compile Tremolo as a shared library.
+# Compile Tremolo (normal precision) as a shared library.
 
 include $(CLEAR_VARS)
 
@@ -31,8 +31,44 @@ LOCAL_SRC_FILES  := bitwise.c      \
                     speed.s        \
                     vorbisfile.c   \
 
+
+# Do not use these, they are for low-precision version.
                     # mdctLARM.s     \
                     # floor1LARM.s   \
+
+LOCAL_CFLAGS     := -ffast-math -D_ARM_ASSEM_
+
+include $(BUILD_SHARED_LIBRARY)
+
+########################################################################################################
+# Compile Tremolo (low precision) as a shared library.
+
+include $(CLEAR_VARS)
+
+TARGET_PLATFORM := android-3
+
+LOCAL_MODULE     := tremolo-low-precision
+LOCAL_ARM_MODE   := arm
+LOCAL_PATH       := $(ROOT_PATH)/tremolo
+LOCAL_SRC_FILES  := bitwise.c      \
+                    bitwiseARM.s   \
+                    codebook.c     \
+                    dpen.s         \
+                    dsp.c          \
+                    floor0.c       \
+                    floor1.c       \
+                    floor1LARM.s   \
+                    floor_lookup.c \
+                    framing.c      \
+                    info.c         \
+                    mapping0.c     \
+                    mdct.c         \
+                    mdctLARM.s     \
+                    misc.c         \
+                    res012.c       \
+                    speed.s        \
+                    vorbisfile.c   \
+
 
 LOCAL_CFLAGS     := -ffast-math -D_ARM_ASSEM_
 
